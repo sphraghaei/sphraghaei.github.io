8fa3ce14c3d1854499d16cbaf11e21cb6b7bf4f3
sphraghaei <86032246+sphraghaei@users.noreply.github.com>
2021-07-22 19:55:57 +0430

    Update index.js

--- mu4/index.js
+++ mu4/index.js
@@ -1,12 +1,4 @@
-(function (window) {
-    var speakWord = "Hello";
-    var helloSpeaker = function (name) {
-        console.log(speakWord + " " + name);
-    }
 
-    window.helloSpeaker = helloSpeaker;
-
-})(window);
 (function (window) {
     var speakWord = "Good Bye";
     var byeSpeaker = function (name) {
