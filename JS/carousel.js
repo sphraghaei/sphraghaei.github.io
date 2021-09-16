71cae1ff324cff51eaac43ba25ee48e5713398bf
sphraghaei <86032246+sphraghaei@users.noreply.github.com>
2021-09-17 00:37:27 +0430

    Add files via upload

---
+++ JS/carousel.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
